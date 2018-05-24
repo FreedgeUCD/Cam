4eb221815f3112fca28e13302878470d2c5bda0e
vegasood <vsood@ucdavis.edu>
2018-05-23 20:13:39 -0700

    Uploads captured pictures to my Amazon AWS Bucket
    TODO:
    
    Work on setting up the server + uploading pictures there

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
-#ref https://stackoverflow.com/questions/34588464/python-how-to-capture-image-from-webcam-on-click-using-opencv
+#ref https://github.com/keithweaver/python-aws-s3/blob/master/example.py
 
 import cv2
+import boto3
+from botocore.client import Config
+
+ACCESS_KEY_ID = ''
+ACCESS_SECRET_KEY = ''
+BUCKET_NAME = ''
 
 cam = cv2.VideoCapture(1)
 
 cv2.namedWindow("test")
 
 img_counter = 0
-
 
 ret, frame = cam.read()
 cv2.imshow("test", frame)
@@ -21,3 +26,18 @@ img_counter += 1
 cam.release()
 
 cv2.destroyAllWindows()
+
+print ("Initializing Image Upload...\n")
+
+data = open(img_name, 'rb')
+
+s3 = boto3.resource(
+    's3',
+    aws_access_key_id=ACCESS_KEY_ID,
+    aws_secret_access_key=ACCESS_SECRET_KEY,
+    config=Config(signature_version='s3v4')
+)
+s3.Bucket(BUCKET_NAME).put_object(Key=img_name, Body=data)
+
+print ("Done")
+
